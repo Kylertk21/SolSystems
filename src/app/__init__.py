a47f370576712eabd41385d56586f0509ff8dea7
Sho <svang14@msudenver.edu>
2024-03-09 16:30:10 -0700

    hardcode

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,5 +4,9 @@ Instructor: Thyago Mota
 Team:
 Description: Project 1 - Sol Systems Web App
 '''
-
+# hard code 
+user1 = User(id=1, name='John ', passwd=b'my_secure_password')
+order1 = Order(number=21, creation_date='2024-03-09', items='ProductA, ProductB', status='Pending')
+product1 = Product(code=1, description='ProductA', availability=True, price=10)
+item1 = Item(sequential_number=1, quantity=2, paid_price=20.0)
 pass
