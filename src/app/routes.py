a35a6761818a6e46e035e8ca8fe3b61a5651eccf
munirasultan <msultan1@msudenver.edu>
2024-03-13 15:54:28 -0600

    complete

--- src/app/routes.py
+++ src/app/routes.py
@@ -2,6 +2,24 @@ from flask import render_template, redirect, url_for, request, session
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User, Product, Order 
 
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+users = {
+    1: {'id': 'tmota', 'username': 'tmota', 'password': '1', 'is_admin': True}
+}
+
+products = {
+    1: {'code': 101, 'description': '6x8 monocrystalline cell panel, 240W', 'availability': True, 'price': 150.00},
+    2: {'code': 202, 'description': '6x10 monocrystalline cell panel, 310W', 'availability': True, 'price': 300.00},
+    3: {'code': 303, 'description': '6x12 monocrystalline cell panel, 400W', 'availability': True, 'price': 450.00}
+}
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -32,3 +50,34 @@ def place_order():
     else:
         products = Product.query.all()
         return render_template('place_order.html', products=products)
+    
+@app.route('/track_order/<order_number>')
+@login_required
+def track_order(order_number):
+    order = next((o for o in orders.values() if o['number'] == order_number), None)
+    if order and (current_user.is_admin or order['customer_id'] == current_user.id):
+        return render_template('track_order.html', order=order)
+    else:
+        return redirect(url_for('dashboard'))
+    
+@app.route('/update_catalog', methods=['GET', 'POST'])
+@login_required
+def update_catalog():
+    if current_user.is_admin:
+        if request.method == 'POST':
+            return redirect(url_for('admin_dashboard'))
+        else:
+            return render_template('update_catalog.html', products=products.values())
+    else:
+        return redirect(url_for('dashboard'))    
+
+@app.route('/change_order_status/<order_number>', methods=['POST'])
+@login_required
+def change_order_status(order_number):
+    if current_user.is_admin:
+        return redirect(url_for('admin_dashboard'))
+    else:
+        return redirect(url_for('dashboard'))
+
+    if __name__ == '__main__':
+        app.run(debug=True)
