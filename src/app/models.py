f75b7ba48f5d5dd414ea766611a9f91f73dacaa1
Sho <svang14@msudenver.edu>
2024-03-13 10:08:47 -0600

    Updated_Version_Of_Mods

--- src/app/models.py
+++ src/app/models.py
@@ -7,18 +7,19 @@ class User(db.Model, UserMixin):
     # customer and admin can just be objects of this class we just need to figure a way how to difrental betwwen the two
     
 class Order():
-    number = int 
-    creation_date = String 
-    items = String # should this be a string of products ?
-    status = String 
+    number = db.Column(db.int, primary_key=True)
+    creation_date = db.Column(db.String)
+    item = db.Column(db.String) #do we need this one ?
+    satus = db.Column(db.String)
 
 class Product():
-    code = int 
-    description = String
-    availability = False
-    price = int
+    code = db.Column(db.int, primary_key=True)
+    description = db.Column(db.String)
+    availability = db.Colum(db.models.BinaryField(_("False")))
+    price = db.Column(db.int, primary_key=True)
+
     
 class Item():
-    sequential_number = int 
-    quantity = int 
-    paid_price = double 
+    sequential_number = db.Column(db.int, primary_key=True)
+    quantity = db.Column(db.int, primary_key=True)
+    paid_price = db.Column(db.LargeBinary)
